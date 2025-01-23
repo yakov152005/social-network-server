c12e0273e00342ff4b32ee284452da7bf5536eb5
yakov152005 <yakov152005@gmail.com>
2025-01-23 16:13:25 +0200

    פרודקשיין

---
+++ src/main/java/org/server/socialnetworkserver/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.server.socialnetworkserver.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css|\\.png|\\.jpg|\\.jpeg|\\.gif|\\.svg|\\.woff|\\.woff2|\\.ttf|\\.eot|\\.ico)$}")
+                .setViewName("forward:/index.html");
+    }
+}
