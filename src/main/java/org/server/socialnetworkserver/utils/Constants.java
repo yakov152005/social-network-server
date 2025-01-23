f39f1d48178ed5f2812e4c2b601c825b70c67561
yakov152005 <yakov152005@gmail.com>
2025-01-23 14:30:00 +0200

    פרודקשיין

--- src/main/java/org/server/socialnetworkserver/utils/Constants.java
+++ src/main/java/org/server/socialnetworkserver/utils/Constants.java
@@ -8,13 +8,18 @@ public class Constants {
     public static Dotenv dotenv = Dotenv.load();
 
     public static class ForTest{
+        public static final String URL_TEST = "http://localhost:8080/social-network/slow-endpoint";
+        /*
         public static final String URL_TEST = dotenv.get("URL_TEST");
+         */
     }
 
     public static class ChatGpt{
-        public static final String ID_GPT = EnvironmentConfig.getProperty("ID_GPT");
-        public static final String SEND_MESSAGE = EnvironmentConfig.getProperty("PATH_SEND_MESSAGE");
+        public static final String ID_GPT = "206263667";
+        public static final String SEND_MESSAGE = "https://app.seker.live/fm1/send-message";
         /*
+         public static final String ID_GPT = EnvironmentConfig.getProperty("ID_GPT");
+        public static final String SEND_MESSAGE = EnvironmentConfig.getProperty("PATH_SEND_MESSAGE");
         public static final String ID_GPT = dotenv.get("ID_GPT");
         public static final String SEND_MESSAGE = dotenv.get("PATH_SEND_MESSAGE");
          */
@@ -28,20 +33,18 @@ public class Constants {
          public static final String URL_CLIENT_PC = dotenv.get("URL_CLIENT_PC");
          public static final String URL_CLIENT_PC = EnvironmentConfig.getProperty("URL_CLIENT_PC");
          */
-
     }
 
     /*
-
     public static class DataBase{
         public static final String DB_HOST = EnvironmentConfig.getProperty("DB_HOST");
         public static final String DB_USER = EnvironmentConfig.getProperty("DB_USER");
         public static final String DB_PASSWORD = EnvironmentConfig.getProperty("DB_PASSWORD");
         public static final String DB_NAME = EnvironmentConfig.getProperty("DB_NAME");
         spring.datasource.url=${DB_URL}
-spring.datasource.username=${DB_USERNAME}
-spring.datasource.password=${DB_PASSWORD}
-spring.datasource.name=${DB_NAME}
+        spring.datasource.username=${DB_USERNAME}
+        spring.datasource.password=${DB_PASSWORD}
+        spring.datasource.name=${DB_NAME}
     }
      */
 
@@ -49,9 +52,11 @@ spring.datasource.name=${DB_NAME}
         /*
         public static final String SENDER_EMAIL = dotenv.get("SENDER_EMAIL");
         public static final String SENDER_PASSWORD = dotenv.get("SENDER_PASSWORD");
-         */
-        public static final String SENDER_EMAIL = EnvironmentConfig.getProperty("SENDER_EMAIL");
+         public static final String SENDER_EMAIL = EnvironmentConfig.getProperty("SENDER_EMAIL");
         public static final String SENDER_PASSWORD = EnvironmentConfig.getProperty("SENDER_PASSWORD");
+         */
+        public static final String SENDER_EMAIL = "servicenetwork62@gmail.com";
+        public static final String SENDER_PASSWORD = "sinh araw dtpo vqoe";
         public static final String PERSONAL = "Social Network";
         public static final String TITLE = "Hey From Social Network";
         public static final String CONTENT = "לא התחברת המון זמן, בוא לבקר אותנו :)";
@@ -61,9 +66,10 @@ spring.datasource.name=${DB_NAME}
 
     public static class SmsConstants{
         public static final String SMS_SENDER = "NETWORKAPI";
-        public static final String SMS_TOKEN = EnvironmentConfig.getProperty("SMS_TOKEN");
+        public static final String SMS_TOKEN = "c2hhaUBlbGVjdG9yLmNvLmlsOmUxYjc3YjhiLWYzZjctNGFlMC1hNzYzLTY3ZDk4YmE5YTJlNQ";
         public static final String URL_SMS = "https://capi.inforu.co.il/api/v2/SMS/SendSms";
         /*
+        public static final String SMS_TOKEN = EnvironmentConfig.getProperty("SMS_TOKEN");
         public static final String SMS_TOKEN = env.getProperty("SMS_TOKEN");
         public static final String URL_SMS = env.getProperty("URL_SMS");
          */
